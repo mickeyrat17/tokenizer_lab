bd43a2de50ab3baf55e32f1ba68a24560429ac8a
mickeyrat17 <dcmoreno17@gmail.com>
2021-09-09 12:21:31 -0600

    Added user input prompt to main method. Added method to count the number of characters in the inputted string (chars, spaces, tabs, zero-terminator included)

--- tokenizer.c
+++ tokenizer.c
@@ -1,6 +1,25 @@
-#include <"stdio.h">
+#include <stdio.h>
+
+int count_chars(char *word);
+
+
+//counts the number of chars (spaces and words) in the inputted string
+//counts the zero terminator at the end of the string as well
+int count_chars(char *word)
+{
+  int count = 0;
+  while (*word != '\0'){
+    word++;
+    count++;
+  }
+  return count;
+}
 
 int main()
 {
-
+  char word[50];
+  printf("Enter a string to be counted:\n");
+  fgets(word, sizeof(word), stdin);
+  printf("Number of chars = %i\n",count_chars(word));
+  return 0;
 }
