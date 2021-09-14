a75ad7de8a88ef1aaee14fbe93b6b6e7bf6d9914
mickeyrat17 <dcmoreno17@gmail.com>
2021-09-14 11:06:30 -0600

    Added implementation for delim_character, non-delim_character, *start_word, *end_word, and count_word.

--- tokenizer.c
+++ tokenizer.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+bool delim_character(char c);
+bool non_delim_character(char c);
+char *word_start(char* str);
+char *end_word(char* str);
+int count_tokens(char* str);
 
 int count_chars(char *word);
 
@@ -7,37 +14,53 @@ int count_chars(char *word);
   Zero terminators are not printable (therfore false) */
 bool delim_character(char c)
 {
-
+  if(c==' ' || c == '\t' || c=='\n') return true;
+  return false;
 }
 
 /*Return true (non-zero) if c is a non-whitespace
   character (not tab or space).
   Zero terminators are not printable (therefore false)*/
-
 bool non_delim_character(char c)
 {
-
+  if(delim_character(c) || c=='\0') return false;
+  return true;
 }
 
 /*Returns a pointer to the first character of the next
   space-separated word*/
 char *word_start(char* str)
 {
-
+  while (*str != '\0') {
+    char curr_char = str[0];
+    if(delim_character(curr_char) && non_delim_character(str[1])) {
+      *str++;
+      break;
+    }
+    *str++;
+  }
+  return str;
 }
 
 /*Returns a pointer to the first space character of the zero
   terminated string*/
 char *end_word(char* str)
 {
-
+  while(delim_character(*str)) str++;
+  return str;
 }
 
 // counts the number of words or tokens
 int count_tokens(char* str)
 {
-
+  int count = 0;
+  while(*str != '\0') {
+    str = word_start(str);
+    count++;
+  }
+  return count;
 }
+
 /* Returns a freshly allocated zero-terminated vector of freshly allocated
    space-separated tokens from zero-terminated str.
    For example, tokenize("hello world string") would result in:
@@ -46,6 +69,7 @@ int count_tokens(char* str)
       tokens[2] = "string"
       tokens[3] = 0
 */
+/*
 char *copy_str(char *inStr, short len)
 {
 
@@ -60,6 +84,7 @@ void print_all_tokens(char** tokens)
 {
 
 }
+*/
 
 //counts the number of chars (spaces and words) in the inputted string
 //counts the zero terminator at the end of the string as well
@@ -78,6 +103,6 @@ int main()
   char word[50];
   printf("Enter a string to be counted:\n");
   fgets(word, sizeof(word), stdin);
-  printf("Number of chars = %i\n",count_chars(word));
+  printf("Number of words = %i\n",count_tokens(word));
   return 0;
 }
